ec90ebb54a6d84b257119305a86ba4d4daebda0f
Faisal Aziz <faziz6006@gmail.com>
2019-12-12 15:23:07 -0600

    day 2 addicitons

--- src/componants/Body.js
+++ src/componants/Body.js
@@ -1,13 +1,27 @@
 import React from "react";
+import Styled from "styled-components";
+
+
+
 
 export default function Body(props) {
-console.log (props.data)
+console.log (props.data);
     
-        return (<div>
-            <h1>Daily Picture</h1>
+
+
+const StyledDiv = Styled.div`
+background: green`;
+ const StyleHeading = Styled.h1`
+ color: pink`
+ const styleHeading = Styled.h3`
+ color:purple`
+
+
+        return (<StyledDiv>
+            <StyleHeading>Daily Picture</StyleHeading>
             <img src= {props.data.url} alt="Smiley face" height="500" width="500"/>
-         <h3>{props.data.explanation}</h3>
-         </div>
+         <StyleHeading>{props.data.explanation}</StyleHeading>
+         </StyledDiv>
         )
 
       
